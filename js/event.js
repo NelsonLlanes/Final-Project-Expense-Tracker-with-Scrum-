4f7a467fb8df4a9098894c07bd859b8e17d52dd5
Matheus Paternez <matheusgfpaternez2006@gmail.com>
2025-10-12 13:12:02 -0700

    Add js logic

--- js/event.js
+++ js/event.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Logic for opening the modal form
+    const modal = document.getElementById('modal-add-transaction');
+    const openBtn = document.getElementById('btn-open-modal');
+    const closeBtn = document.getElementById('btn-close-modal');
+    
+        openBtn.addEventListener('click', () => {
+            modal.classList.remove('hidden'); // Remove 'hidden' to show the modal form
+        });
+
+    // Logic for closing the modal
+        closeBtn.addEventListener('click', () => {
+            modal.classList.add('hidden'); // Add 'hidden' to hide the modal form
+        });
+
     const form = document.getElementById('expense-form');
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -15,8 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
         if (addExpense(expense)) {
             form.reset();
             alert('Expense added successfully!');
+            document.getElementById('modal-add-transaction').classList.add('hidden'); // For closing the modal when something is added
         } else {
             alert('Failed to add expense. Please try again.');
         }
     });
 });
+
