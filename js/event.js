bc20a0350a414b9a08c339262d862dc986b495d8
Nelson Llanes <nelsoncanada23@gmail.com>
2025-10-12 22:29:00 -0700

    add erase option and render list database

--- js/event.js
+++ js/event.js
@@ -1,22 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('expense-form');
+    renderExpenses();
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        
+
+
+
         const expense = {};
         expense.id = crypto.randomUUID();
         expense.createdAt = new Date().toISOString();
-        expense.name = document.getElementById('name').value;
+        // expense.name = document.getElementById('name').value;
+        expense.type = document.getElementById('type').value;
+        expense.category = document.getElementById('category').value;
         expense.amount = parseFloat(document.getElementById('amount').value);
         expense.note = document.getElementById('note').value;
-        expense.category = document.getElementById('category').value;
-        expense.occurredAt = document.getElementById('occurred-at').value;
-        
+        expense.occurredAt = document.getElementById('location').value;
+
         if (addExpense(expense)) {
             form.reset();
-            alert('Expense added successfully!');
+            renderExpenses();
         } else {
             alert('Failed to add expense. Please try again.');
         }
     });
 });
+
